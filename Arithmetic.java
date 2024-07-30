9177a521ffdb739700bbc758246c7d93c57b8dd9
karesomesh15 <karesomesh15@gmail.com>
2024-07-30 14:48:56 +0530

    this is day2 java programs

---
+++ Arithmetic.java
@@ -0,0 +1,28 @@
+public class Arithmetic{
+
+public int add(int num1, int num2){
+
+return num1+num2;
+}
+
+public int sub(int num1, int num2){
+
+return num1-num2;
+}
+
+public int mul(int num1, int num2){
+    return num1*num2;
+}
+public int div(int num1, int num2){
+    return num1/num2;
+}
+}
+
+
+
+
+
+
+
+
+
